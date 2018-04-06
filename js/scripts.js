afa9c69fa1507fc4ea6d60d06ee4dfb1ff8bf4c5
Guest User <Guest@epicodus-19.local>
2018-04-06 11:44:12 -0700

    add address section to allow to type in address where pizza will be delivered

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,26 @@
 
 pizzaPrice = 0;
 
-function Pizza (name, numberOfToppings, size, toppingsList) {
+function Pizza(name, numberOfToppings, size, toppingsList) {
   this.pizzaName = name;
   this.pizzaToppingNumber = numberOfToppings;
   this.pizzaSize = size;
   this.pizzaToppingsList = toppingsList;
+  this.pizzaAddress = [];
+}
+
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
 }
 
 Pizza.prototype.PizzaName = function() {
   return  this.pizzaSize + " " + this.pizzaName;
+}
+
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state;
 }
 
 Pizza.prototype.getPrice = function() {
@@ -28,8 +39,13 @@ Pizza.prototype.getPrice = function() {
 
 function resetFields() {
     $("input#pizzaName").val("");
-    $("input:checkbox[name=topping]").val("");
-    $("#size-of-pizza").val("");
+    $("#size-of-pizza").val("small");
+    $('input[type=checkbox]').each(function() {
+        this.checked = false;
+    });
+    $("input.street").val("");
+    $("input.city").val("");
+    $("input.state").val("");
 }
 
 $(document).ready(function() {
@@ -37,22 +53,31 @@ $(document).ready(function() {
     event.preventDefault();
 
     var inputPizzaName = $("#pizzaName").val();
+    var capitalizeFirstElement = inputPizzaName.charAt(0).toUpperCase() + inputPizzaName.slice(1);
     var inputPizzaSize = $("#size-of-pizza").val();
     var inputNumberOfToppings = 0;
     var inputToppings = [];
-    $("input:checkbox[name=topping]:checked").each(function(){
+    $("input:checkbox[name=topping]:checked").each(function() {
       inputToppings.push($(this).val());
       inputNumberOfToppings++;
     });
 
-    var newPizza = new Pizza(inputPizzaName,inputNumberOfToppings,inputPizzaSize,inputToppings);
+    var newPizza = new Pizza(capitalizeFirstElement,inputNumberOfToppings,inputPizzaSize,inputToppings);
     var calculatedPizzaPrice = newPizza.getPrice();
+
+    var inputStreet = $("input.street").val();
+    var inputCity = $("input.city").val();
+    var inputState = $("input.state").val();
+    var newAddress = new Address(inputStreet, inputCity, inputState);
+    newPizza.pizzaAddress.push(newAddress);
+
+    console.log(newAddress);
+    console.log(newPizza);
 
     $("ul#ordered-pizza-list").append("<li><span class='ordered-pizza'>" + newPizza.PizzaName() + "</span></li>");
 
     resetFields();
 
   });
-
 
 });
